c83354a2d6f7e51de20c83bc66775b3ea66bf5d5
Salazar-prime <aggarw82@purdue.edu>
2019-09-14 15:32:26 -0400

    complete softmax_loss_naive

--- HW1/ie590/classifiers/softmax.py
+++ HW1/ie590/classifiers/softmax.py
@@ -36,7 +36,29 @@ def softmax_loss_naive(W, X, y, reg):
     #############################################################################
     #                          START OF YOUR CODE                               #
     #############################################################################
-    pass ## Write your code here
+    #pass ## Write your code here
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
+    
+    for i in range(num_train):
+        scores = X[i].dot(W)
+        scores -= np.max(scores) # prevetn overflow during exp()
+        
+        prob_sum = np.sum(np.exp(scores))
+        current_prob = np.exp(scores[y[i]]) / prob_sum
+        loss -= np.log(current_prob)
+        
+        for j in range(num_classes):
+            if y[i] != j:
+                dW[:,j] += X[i] * np.exp(scores[j]) / prob_sum
+        dW[:,y[i]] +=  (current_prob -1) * X[i]
+    
+    # average    
+    loss /= num_train
+    dW /= num_train
+    # add regularization
+    loss += reg * np.sum(W*W)
+    dW += 2*W*reg
     #############################################################################
     #                          END OF YOUR CODE                                 #
     #############################################################################
